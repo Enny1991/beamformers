df51a5a62ac837807ad82de64c91ccd4107d507c
Enea Ceolini <enea.ceolini@gmail.com>
2019-01-03 18:20:01 +0100

    WIP tests td

--- test_beamformers.py
+++ test_beamformers.py
@@ -6,7 +6,7 @@ import numpy as np
 import warnings
 import soundfile as sf
 
-from beamformers import MVDR, SDW_MWF
+from beamformers import MVDR, SDW_MWF, MWF_Oracle, MSNR, TD_MVDR, TD_MWF
 
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
@@ -14,7 +14,7 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 class TestDeepExplainGeneralTF(TestCase):
 
     def setUp(self):
-        print("starting")
+
         mix, fs = sf.read('wavs/mix.wav')
         self.mix = mix.T
         nn, fs = sf.read('wavs/nn.wav')
@@ -25,37 +25,115 @@ class TestDeepExplainGeneralTF(TestCase):
         self.gt = gt
 
         fs = 8000
-        receptive_field = 0.064  # in s
+        receptive_field = 0.128  # in s
         self.frame_len = int(fs * receptive_field)
         self.frame_step = int(self.frame_len / 4)
 
-    def tearDown(self):
-        print("done")
+    # def tearDown(self):
+    #     print("done")
+    
+    def test_td_mwf_wref(self):
 
-    def test_mvdr(self):
+        true_td_mwf_wref, fs = sf.read('wavs/td_mwf_wref.wav')
+
+        out_td_mwf_wref = TD_MWF(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_td_mwf_wref, 8000)
+        out_td_mwf_wref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_td_mwf_wref, true_td_mwf_wref)
+
+    def test_td_mwf_nref(self):
+        true_td_mwf_nref, fs = sf.read('wavs/td_mwf_nref.wav')
+
+        out_td_mwf_nref = TD_MWF(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_td_mwf_nref, 8000)
+        out_td_mwf_nref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_td_mwf_nref, true_td_mwf_nref)
+    
+    def test_td_mvdr_wref(self):
+
+        true_td_mvdr_wref, fs = sf.read('wavs/td_mvdr_wref.wav')
+
+        out_td_mvdr_wref = TD_MVDR(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_td_mvdr_wref, 8000)
+        out_td_mvdr_wref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_td_mvdr_wref, true_td_mvdr_wref)
+
+    def test_td_mvdr_nref(self):
+        true_td_mvdr_nref, fs = sf.read('wavs/td_mvdr_nref.wav')
+
+        out_td_mvdr_nref = TD_MVDR(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_td_mvdr_nref, 8000)
+        out_td_mvdr_nref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_td_mvdr_nref, true_td_mvdr_nref)
+        
+    def test_sdw_mwf_wref(self):
+
+        true_sdw_mwf_wref, fs = sf.read('wavs/sdw_mwf_wref.wav')
+
+        out_sdw_mwf_wref = SDW_MWF(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_sdw_mwf_wref, 8000)
+        out_sdw_mwf_wref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_sdw_mwf_wref, true_sdw_mwf_wref)
+
+    def test_sdw_mwf_nref(self):
+        true_sdw_mwf_nref, fs = sf.read('wavs/sdw_mwf_nref.wav')
+
+        out_sdw_mwf_nref = SDW_MWF(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_sdw_mwf_nref, 8000)
+        out_sdw_mwf_nref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_sdw_mwf_nref, true_sdw_mwf_nref)
+        
+    def test_msnr_wref(self):
+
+        true_msnr_wref, fs = sf.read('wavs/msnr_wref.wav')
+
+        out_msnr_wref = MSNR(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_msnr_wref, 8000)
+        out_msnr_wref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_msnr_wref, true_msnr_wref)
+
+    def test_msnr_nref(self):
+        true_msnr_nref, fs = sf.read('wavs/msnr_nref.wav')
+
+        out_msnr_nref = MSNR(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_msnr_nref, 8000)
+        out_msnr_nref, fs = sf.read('wavs/test_out.wav')
+
+        np.testing.assert_equal(out_msnr_nref, true_msnr_nref)
+
+    def test_mvdr_wref(self):
 
         true_mvdr_wref, fs = sf.read('wavs/mvdr_wref.wav')
-        true_mvdr_nref, fs = sf.read('wavs/mvdr_nref.wav')
 
         out_mvdr_wref = MVDR(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
-        out_mvdr_wref /= np.max(np.abs(out_mvdr_wref))
-        out_mvdr_nref = MVDR(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_mvdr_wref, 8000)
+        out_mvdr_wref, fs = sf.read('wavs/test_out.wav')
 
-        # self.assertEqual(out_mvdr_wref, true_mvdr_wref)
-        # self.assertEqual(out_mvdr_nref, true_mvdr_nref)
         np.testing.assert_equal(out_mvdr_wref, true_mvdr_wref)
+
+    def test_mvdr_nref(self):
+        true_mvdr_nref, fs = sf.read('wavs/mvdr_nref.wav')
+
+        out_mvdr_nref = MVDR(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_mvdr_nref, 8000)
+        out_mvdr_nref, fs = sf.read('wavs/test_out.wav')
+
         np.testing.assert_equal(out_mvdr_nref, true_mvdr_nref)
 
     def test_sdw_mwf(self):
 
-        true_sdw_mwf_wref = sf.read('wavs/sdw_wref.wav')
-        true_sdw_mwf_nref = sf.read('wavs/sdw_nref.wav')
+        true_mwf, fs = sf.read('wavs/mwf.wav')
 
-        out_sdw_mwf_wref = SDW_MWF(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
-        out_sdw_mwf_nref = SDW_MWF(self.mix, self.nn, reference=None, frame_len=self.frame_len, frame_step=self.frame_step)
-
-        self.assertEqual(out_sdw_mwf_wref, true_sdw_mwf_wref)
-        self.assertEqual(out_sdw_mwf_nref, true_sdw_mwf_nref)
-        np.testing.assert_equal(out_sdw_mwf_wref, true_sdw_mwf_wref)
-        np.testing.assert_equal(out_sdw_mwf_nref, true_sdw_mwf_nref)
+        out_mwf = MWF_Oracle(self.mix, self.nn, self.spk, frame_len=self.frame_len, frame_step=self.frame_step)
+        sf.write('wavs/test_out.wav', out_mwf, 8000)
+        out_mwf, fs = sf.read('wavs/test_out.wav')
+        
+        np.testing.assert_equal(out_mwf, true_mwf)
 
