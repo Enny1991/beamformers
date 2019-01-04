30d56d17fbedef6321a39c93e22ddcafcab440e9
Enea Ceolini <enea.ceolini@gmail.com>
2019-01-04 14:18:44 +0100

    added setup

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(name='beamformers',
+      version='0.1',
+      description='Beamformers for audio source separation and speech enhancement',
+      url='https://github.com/Enny1991/beamformers',
+      author='Enea Ceolini (UZH Zurich)',
+      author_email='enea.ceolini@ini.uzh.ch',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+            'scipy',
+            'numpy',
+            'soundfile'
+      ],
+      # extras_require={
+      #       "tf": ["tensorflow>=1.0.0"],
+      #       "tf_gpu": ["tensorflow-gpu>=1.0.0"],
+      # },
+      zip_safe=False,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      )
